aca53e2551a12e2c8a8145b9ba3e5719c336cdb2
janakripan <janakripanjkc@gmail.com>
2024-12-03 11:11:30 +0530

    completed

---
+++ src/components/Graphics.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import Finance from '../assets/Finance.png'
+import crm from "../assets/crm.png"
+import profile from '../assets/profile.png'
+import graph from '../assets/graph.png'
+import barGraph from '../assets/barGraph.png'
+
+function Graphics() {
+  return (
+    <div className="graphics hidden md:block md:opacity-50 lg:opacity-100 w-[1000px] h-[1000px] absolute right-[-400px] z-0 top-[-343px] 2xl:right-[500] ">
+              
+             <div className=' w-full h-full relative '>
+
+             <div
+                className="ellipse w-[1000px] h-[1000px] bg-[#BD93BD99]
+                opacity-60 rounded-full   ">
+                
+              </div>
+              <img
+                  src={crm}
+                  className=" w-[358px] h-[468px] absolute right-[449px] top-[482px] z-30 "
+                  alt=""/>
+
+                <div className=" hidden lg:block lg:opacity-75 xl:opacity-100 finance w-fit h-fit bg-white z-30 absolute right-[830px] top-[585px] border-[.36px] border-[#CFD8DC] rounded-xl px-1.5 ">
+
+                    <img src={Finance} className='h-auto w-auto ' alt="" />
+
+                  
+                </div>
+
+                <img src={profile} className='z-40 absolute w-auto h-auto right-[435px] top-[416px] ' alt="" />
+
+                <img src={barGraph} className=' hidden lg:block z-40 w-auto h-auto absolute  right-[639px] top-[817px] ' alt="" />
+                <img src={graph} className=' w-auto h-auto absolute right-[400px] top-[650px] z-40  ' alt="" />
+
+
+             </div>
+
+            </div>
+  )
+}
+
+export default Graphics
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,23 +1,20 @@
 import circltick from "../assets/charm_circle-tick.png";
 import arrow from "../assets/arrow-right.png";
 import PurchaseCard from "../components/PurchaseCard";
-import dp from "../assets/dp.jpeg";
-import crm from "../assets/crm.png";
+
 import Section2 from "../components/Section2";
 import Section3 from "../components/Section3";
+import Graphics from "../components/Graphics";
 
 export default function Home() {
   return (
     <>
       <main className=" w-full  ">
         <section className="">
-          <div
-            className="hero h-fit w-full py-6  bg-[#F2EDEB] md:py-16
-                     md:pl-16 md:h-[680px] overflow-hidden flex relative "
-          >
-            <div className="flex flex-col items-center md:items-start gap-y-6 md:w-1/2">
-              <h1 className=" text-4xl  md:text-5xl xl:text-6xl  font-bold">
-                <span className="lg:leading-[90px]">
+          <div className="hero h-fit w-full py-6  bg-[#F2EDEB] md:py-16 md:pl-16 md:h-[680px] overflow-hidden flex relative z-50 ">
+            <div className="flex flex-col items-center md:items-start gap-y-6 md:w-1/2 z-50 ">
+              <h1 className=" text-4xl  md:text-5xl xl:text-6xl  font-bold z-50 ">
+                <span className="lg:leading-[90px] z-50 ">
                   Connecting Retailers To Their Coustomers Like
                 </span>{" "}
                 <br />
@@ -40,7 +37,7 @@ export default function Home() {
                     className="w-3 h-3 md:h-5 md:w-5"
                     alt=""
                   />
-                  Asmarter way to shwcase and sell your products.
+                  Asmarter way to showcase and sell your products.
                 </li>
                 <li className="flex flex-row justify-start items-start gap-3">
                   <img
@@ -60,38 +57,11 @@ export default function Home() {
                   View Products <img className=" h-6 w-6" src={arrow} alt="" />
                 </button>
               </div>
-            </div>
-            <div className="graphics   ">
-              {/* <img src={ellipse} className='absolute right-[0px] top-[-103px] z-0 ' alt="" /> */}
-              <div
-                className="ellipse w-[1000px] h-[1000px] bg-[#BD93BD99]
-                         opacity-60 rounded-full absolute right-[-300px] top-[-343px] "
-              >
-                <img
-                  src={crm}
-                  className=" w-[358px] h-[468px] absolute right-[449px] top-[482px] z-50 "
-                  alt=""
-                />
+            </div >
 
-                <div className="finance w-[150px] h-[325px] opacity-100 bg-white z-50 absolute right-[830px] top-[585px] border-[.36px] border-[#CFD8DC] rounded-xl px-1.5 ">
-                  <div>
-                    <div>
-                      <div>
-                        <div className=" w-8 h-1.5 bg-black rounded-xl "></div>
-                      </div>
-                      <div></div>
-                    </div>
-                    <div className="w-5 h-5 rounded-full ">
-                      <img
-                        className=" rounded-full width-full h-full "
-                        src={dp}
-                        alt=""
-                      />
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
+
+            <Graphics/>
+            
           </div>
         </section>
 
